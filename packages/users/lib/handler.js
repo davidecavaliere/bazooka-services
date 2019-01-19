0d1cde301e3ebb4d8102b97ec57194b6dd36dfff
Davide Cavaliere <cavaliere.davide@gmail.com>
2019-01-19 16:49:28 +0000

    builds

--- packages/users/lib/handler.js
+++
@@ -1,3 +0,0 @@
-var apigator_1 = require('@microgamma/apigator');
-var user_service_1 = require('./user.service');
-module.exports = apigator_1.bootstrap(user_service_1.UserService);
--- packages/users/lib/index.js
+++
@@ -1,2 +0,0 @@
-var user_service_1 = require('./user.service');
-module.exports = user_service_1.UserService;
